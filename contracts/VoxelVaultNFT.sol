ed4a64c801b094b886ff257f4009940c822438d0
crypto-mint <hartensteindominic@gmail.com>
2026-08-19 19:34:54 -0400

    Harden NFT mint authorization and preserve creator royalties

--- contracts/VoxelVaultNFT.sol
+++ contracts/VoxelVaultNFT.sol
@@ -9,22 +9,45 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract VoxelVaultNFT is ERC721, ERC721URIStorage, ERC721Royalty, Ownable {
     uint256 private _nextTokenId = 1;
     uint96 public constant MAX_ROYALTY_BPS = 1500;
+    mapping(address => bool) public minters;
 
     event VoxelMinted(uint256 indexed tokenId, address indexed creator, string tokenURI, uint96 royaltyBps);
+    event MinterUpdated(address indexed account, bool allowed);
 
     constructor(address initialOwner) ERC721("Voxel Vault", "VOXEL") Ownable(initialOwner) {}
+
+    modifier onlyMinter() {
+        require(minters[msg.sender] || msg.sender == owner(), "Not authorized to mint");
+        _;
+    }
+
+    function setMinter(address account, bool allowed) external onlyOwner {
+        require(account != address(0), "Invalid minter");
+        minters[account] = allowed;
+        emit MinterUpdated(account, allowed);
+    }
 
     function mint(string calldata uri, uint96 royaltyBps) external returns (uint256 tokenId) {
         return _mintTo(msg.sender, msg.sender, uri, royaltyBps);
     }
 
-    function mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps) external onlyOwner returns (uint256 tokenId) {
+    function mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps)
+        external
+        onlyMinter
+        returns (uint256 tokenId)
+    {
         return _mintTo(recipient, royaltyReceiver, uri, royaltyBps);
     }
 
-    function _mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps) internal returns (uint256 tokenId) {
+    function _mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps)
+        internal
+        returns (uint256 tokenId)
+    {
+        require(recipient != address(0), "Recipient required");
         require(royaltyReceiver != address(0), "Royalty receiver required");
+        require(bytes(uri).length > 0, "Token URI required");
         require(royaltyBps <= MAX_ROYALTY_BPS, "Royalty too high");
+
         tokenId = _nextTokenId++;
         _safeMint(recipient, tokenId);
         _setTokenURI(tokenId, uri);
@@ -37,11 +60,21 @@ contract VoxelVaultNFT is ERC721, ERC721URIStorage, ERC721Royalty, Ownable {
         _burn(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage, ERC721Royalty) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage, ERC721Royalty)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
         return super.tokenURI(tokenId);
     }
 
