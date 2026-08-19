460e65c902714cd43999f969aa52f4e0464f7afe
crypto-mint <hartensteindominic@gmail.com>
2026-08-19 19:26:20 -0400

    Add Hardhat Sepolia configuration

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+module.exports = {
+  solidity: '0.8.24',
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || '',
+      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : []
+    }
+  }
+};
