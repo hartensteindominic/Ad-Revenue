ed1c2aa7f8daa4d97721dcd006bd70641b33711d
crypto-mint <hartensteindominic@gmail.com>
2026-08-19 19:24:58 -0400

    Add ERC-721 voxel NFT contract with per-token metadata and ERC-2981 royalties

---
+++ contracts/VoxelVaultNFT.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721Royalty} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VoxelVaultNFT is ERC721, ERC721URIStorage, ERC721Royalty, Ownable {
+    uint256 private _nextTokenId = 1;
+    uint96 public constant MAX_ROYALTY_BPS = 1500;
+
+    event VoxelMinted(uint256 indexed tokenId, address indexed creator, string tokenURI, uint96 royaltyBps);
+
+    constructor(address initialOwner) ERC721("Voxel Vault", "VOXEL") Ownable(initialOwner) {}
+
+    function mint(string calldata uri, uint96 royaltyBps) external returns (uint256 tokenId) {
+        return _mintTo(msg.sender, msg.sender, uri, royaltyBps);
+    }
+
+    function mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps) external onlyOwner returns (uint256 tokenId) {
+        return _mintTo(recipient, royaltyReceiver, uri, royaltyBps);
+    }
+
+    function _mintTo(address recipient, address royaltyReceiver, string calldata uri, uint96 royaltyBps) internal returns (uint256 tokenId) {
+        require(royaltyReceiver != address(0), "Royalty receiver required");
+        require(royaltyBps <= MAX_ROYALTY_BPS, "Royalty too high");
+        tokenId = _nextTokenId++;
+        _safeMint(recipient, tokenId);
+        _setTokenURI(tokenId, uri);
+        _setTokenRoyalty(tokenId, royaltyReceiver, royaltyBps);
+        emit VoxelMinted(tokenId, royaltyReceiver, uri, royaltyBps);
+    }
+
+    function burn(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+        _burn(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage, ERC721Royalty) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage, ERC721Royalty) {
+        super._burn(tokenId);
+    }
+}
