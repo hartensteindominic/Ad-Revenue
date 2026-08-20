f0041633772a7e353bb827d58e5f19d911c38308
crypto-mint <hartensteindominic@gmail.com>
2026-08-20 17:31:22 -0400

    feat: connect physical discovery to homepage

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@
 import VaultUniverse from './components/VaultUniverse';
 import UniversalEnginePanel from './components/UniversalEnginePanel';
 import SponsoredCollectibles from './components/SponsoredCollectibles';
+import ProximityDiscovery from './components/ProximityDiscovery';
 
 export default function Home() {
   return (
     <>
       <UniversalEnginePanel />
       <VaultUniverse />
+      <ProximityDiscovery />
       <SponsoredCollectibles />
     </>
   );
