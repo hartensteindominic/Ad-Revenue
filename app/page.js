dbc0b8b50a579ca4874de543acb9a80d3d11e238
crypto-mint <hartensteindominic@gmail.com>
2026-08-20 17:28:22 -0400

    feat: surface sponsor-ready collectible layer

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 
 import VaultUniverse from './components/VaultUniverse';
 import UniversalEnginePanel from './components/UniversalEnginePanel';
+import SponsoredCollectibles from './components/SponsoredCollectibles';
 
 export default function Home() {
   return (
     <>
       <UniversalEnginePanel />
       <VaultUniverse />
+      <SponsoredCollectibles />
     </>
   );
 }
