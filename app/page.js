eb860c9cff3596f9a5b85faa5293bf20136b3b20
crypto-mint <hartensteindominic@gmail.com>
2026-08-20 17:19:25 -0400

    Wire sponsored collectible prototype into homepage

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 
 import VaultUniverse from './components/VaultUniverse';
 import UniversalEnginePanel from './components/UniversalEnginePanel';
+import SponsoredCollectibleShowcase from './components/SponsoredCollectibleShowcase';
 
 export default function Home() {
   return (
     <>
       <UniversalEnginePanel />
       <VaultUniverse />
+      <SponsoredCollectibleShowcase />
     </>
   );
 }
