c2d442ebd42c4a4a261bbd7da7c53117e11473c2
crypto-mint <hartensteindominic@gmail.com>
2026-08-21 00:10:53 -0400

    feat(ai): surface Vault Copilot in main experience

--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@
 import VaultFlowNav from './components/VaultFlowNav';
 import VaultUniverse from './components/VaultUniverse';
 import UniversalEnginePanel from './components/UniversalEnginePanel';
+import VaultAICopilot from './components/VaultAICopilot';
 
 export default function Home() {
   return (
     <>
       <VaultFlowNav />
       <UniversalEnginePanel />
+      <VaultAICopilot />
       <VaultUniverse />
     </>
   );
