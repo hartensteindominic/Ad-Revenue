679541d181ac1457a72cacf1c7ca4fda28f253ad
crypto-mint <hartensteindominic@gmail.com>
2026-08-21 01:33:53 -0400

    ux: hide engine diagnostics from consumer homepage

--- app/page.js
+++ app/page.js
@@ -2,13 +2,11 @@
 
 import VaultFlowNav from './components/VaultFlowNav';
 import VaultUniverse from './components/VaultUniverse';
-import UniversalEnginePanel from './components/UniversalEnginePanel';
 
 export default function Home() {
   return (
     <>
       <VaultFlowNav />
-      <UniversalEnginePanel />
       <VaultUniverse />
     </>
   );
