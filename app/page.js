15baf7b3fcb43c4057642fd9dd62c05c6b20c9ad
crypto-mint <hartensteindominic@gmail.com>
2026-08-20 22:09:50 -0400

    feat: add guided home navigation flow

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client';
 
+import VaultFlowNav from './components/VaultFlowNav';
 import VaultUniverse from './components/VaultUniverse';
 import UniversalEnginePanel from './components/UniversalEnginePanel';
 
 export default function Home() {
   return (
     <>
+      <VaultFlowNav />
       <UniversalEnginePanel />
       <VaultUniverse />
     </>
