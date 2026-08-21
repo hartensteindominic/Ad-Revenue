a60be0a7dc47e82b7e4a76ec67643fcebb815055
crypto-mint <hartensteindominic@gmail.com>
2026-08-21 13:12:26 -0400

    feat: make discovery the complete mobile-first home

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
 'use client';
 
 import ObjectDiscoveryShell from './components/ObjectDiscoveryShell';
-import VaultUniverse from './components/VaultUniverse';
 
 export default function Home() {
-  return (
-    <>
-      <ObjectDiscoveryShell />
-      <VaultUniverse />
-    </>
-  );
+  return <ObjectDiscoveryShell />;
 }
